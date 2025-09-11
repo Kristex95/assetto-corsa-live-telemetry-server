e9cdb867d96513a3961f40e3390e3af6da764316
Kristex95 <kristex95@gmail.com>
2025-09-11 22:49:51 +0300

    first commit

---
+++ src/main/java/com/kristex/ac/livedata/AcLivedataAppApplication.java
@@ -0,0 +1,13 @@
+package com.kristex.ac.livedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcLivedataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcLivedataAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kristex/ac/livedata/AcLivedataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristex.ac.livedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcLivedataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
